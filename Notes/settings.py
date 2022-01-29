58841a9f2006204893d1a78d629fa8184ce9fdfb
Edilson Jr <tylerdinomc@gmail.com>
2022-01-29 18:28:05 +0200

    setup

--- Notes/settings.py
+++ Notes/settings.py
@@ -15,9 +15,6 @@ import django_heroku
 
 import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -90,6 +87,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
