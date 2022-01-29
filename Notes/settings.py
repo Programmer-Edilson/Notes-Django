e5e8a892e5e7762a7a953c377c4c8ccf11846b0f
Edilson Jr <tylerdinomc@gmail.com>
2022-01-29 19:01:38 +0200

    setup

--- Notes/settings.py
+++ Notes/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 import django_heroku
 import os
 
-import dj_database_url
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -89,10 +87,6 @@ DATABASES = {
     }
 }
 
-"""
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
